6d8d69b593324f8d64bf9ad4a77d51ea946d9b56
samehbenamor <sameh.benamor@esprit.tn>
2023-07-03 06:53:40 +0200

    Added skills section and projects, everything still in alpha

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import React from 'react';
 import { NavBar } from './Components/NavBar';
 import 'bootstrap/dist/css/bootstrap.css';
-import {Banner } from './Components/Banner';
-
+import { Banner } from './Components/Banner';
+import { Skills } from './Components/Skills';
+import { Projects } from './Components/Projects';
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Banner/>
+      <Skills/>
+      <Projects/>
     </div>
   );
 }
---
+++ src/Components/Projects.js
@@ -0,0 +1,68 @@
+import { Container, Row, Col, Tab } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import Nav from "react-bootstrap/Nav";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+export const Projects = () => {
+  const projects = [
+    {
+      title: "Ggaming",
+      description: "A gaming tournament hosting website.",
+      imgUrl: projImg1,
+    },
+    {
+      title: "GreenT",
+      description:
+        "A website made to encourage people into preserving the environment.",
+      imgUrl: projImg2,
+    },
+  ];
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col>
+            <h2>Projects</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <Tab.Container id="projects-tabs" defaultActiveKey="first">
+              <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                <Nav.Item>
+                  <Nav.Link eventKey="first">Tab one</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Tab two</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third">Tab three</Nav.Link>
+                </Nav.Item>
+              </Nav>
+
+              <Tab.Content>
+                <Tab.Pane eventKey="first">
+                  <Row>
+                    {projects.map((project, index) => {
+                      return <ProjectCard key={index} {...project} />;
+                    })}
+                  </Row>
+                </Tab.Pane>
+                <Tab.Pane eventKey="second">Test</Tab.Pane>
+                <Tab.Pane eventKey="third">Test</Tab.Pane>
+              </Tab.Content>
+            </Tab.Container>
+          </Col>
+        </Row>
+      </Container>
+      <img src={colorSharp2} className="background-image-right" />
+    </section>
+  );
+};
